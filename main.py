8c03c49b1f02b2507e6ecc5a9831d7682565284f
Tommaso Caputi <tommasocaputi85@gmail.com>
2022-11-11 20:23:21 +0100

    fist commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from github import Github
+from dotenv import load_dotenv
+
+load_dotenv()
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+path = os.getenv('FOLDERPATH')
+
+if __name__ == "__main__":
+    repo_name = str(sys.args[1])
+    os.mkdir(path+repo_name)
+    user = Github(username, password).get_user()
+    repo = user.create(repo_name)
+    print(repo_name+" create succefully")
+
+print(a)
