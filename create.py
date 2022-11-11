2d2707be7dc5283e8b1adc2661a5cb5c0f179676
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2022-11-11 22:13:11 +0000

    .

--- create.py
+++ create.py
@@ -8,11 +8,11 @@ username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
 path = os.getenv('FOLDERPATH')
 
+
 if __name__ == "__main__":
-    repo_name = str(sys.args[1])
+    repo_name = str(sys.argv[1])
     os.mkdir(path+repo_name)
     user = Github(username, password).get_user()
     repo = user.create(repo_name)
     print(repo_name+" create succefully")
 
-print(a)
