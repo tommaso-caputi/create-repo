1e55ba365af9249617bae96c5214d6c747950ed4
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2022-11-12 11:43:17 +0100

    Update create.py

--- create.py
+++ create.py
@@ -4,15 +4,15 @@ from github import Github
 from dotenv import load_dotenv
 
 load_dotenv()
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
+token = os.getenv('TOKEN')
 path = os.getenv('FOLDERPATH')
-
 
 if __name__ == "__main__":
     repo_name = str(sys.argv[1])
-    os.mkdir(path+repo_name)
-    user = Github(username, password).get_user()
-    repo = user.create(repo_name)
+    try:
+        os.mkdir(path+repo_name)
+    except:
+        pass
+    user = Github(token).get_user()
+    repo = user.create_repo(repo_name)
     print(repo_name+" create succefully")
-
